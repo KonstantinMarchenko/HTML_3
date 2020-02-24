f965b82946ae40f86d3fa5305e2052a167eefed4
KonstantinMarchenko <54527927+KonstantinMarchenko@users.noreply.github.com>
2020-02-24 15:57:05 +0300

    Have responsive design, display info on page

--- src/js/index.js
+++ src/js/index.js
@@ -11,97 +11,98 @@ let chooseTurn = true;
 let turnCount = 0;
 
 for (let i = 0; i < n; i++) {
-    for (let k = 0; k < n; k++){
-        let a = i * n + k;
-        let button = $('<button class="buttons__button-main" id="btn' + a + '">' + "." + '</button>');
-        button.appendTo("#buttons");
-    }
+  for (let k = 0; k < n; k++) {
+    let a = i * n + k;
+    let button = $('<button class="buttons__button-main" id="btn' + a + '">' + "." + '</button>');
+    button.appendTo("#buttons");
+  }
 
-    let enter = $('<div></div>')
-    enter.appendTo("#buttons");
+  let enter = $('<div></div>')
+  enter.appendTo("#buttons");
 }
 
 $(".buttons__button-main").click(function () {
-    if (chooseTurn){
-        $(this).text('X');
-        currentIndex = parseInt(this.id.substr(3, 3));
-        mainArray[currentIndex] = 1;
-        makeArray();
-        chooseTurn = false;
-    } else {
-        $(this).text('O');
-        currentIndex = parseInt(this.id.substr(3, 3));
-        mainArray[currentIndex] = 0;
-        makeArray();
-        chooseTurn = true;
-    }
+  if (chooseTurn) {
+    $(this).text('X');
+    currentIndex = parseInt(this.id.substr(3, 3));
+    mainArray[currentIndex] = 1;
+    fillArray();
+    chooseTurn = false;
+  } else {
+    $(this).text('O');
+    currentIndex = parseInt(this.id.substr(3, 3));
+    mainArray[currentIndex] = 0;
+    fillArray();
+    chooseTurn = true;
+  }
 
-    $(this).prop('disabled', true);
-    checkArray();
+  $(this).prop('disabled', true);
+  checkArray();
 });
 
 $("#reset").click(function () {
-    turnCount = 0;
-    chooseTurn = true;
-    finalTurnCheck = false;
-    for (let i = 0; i < n * n; i++) {
-        mainArray[i] = 2;
-    }
-    makeArray();
-    $(".buttons__button-main").prop('disabled', false).css('color', 'black').text('.');
+  turnCount = 0;
+  chooseTurn = true;
+  finalTurnCheck = false;
+  for (let i = 0; i < n * n; i++) {
+    mainArray[i] = 2;
+  }
+  fillArray();
+  $(".buttons__button-main").prop('disabled', false).css('color', 'black').text('.');
+  $("#label_1").text('');
 })
 
-for (let i = 0; i < 2 * n + n; i++){
-    if (!secondaryArray[i]) secondaryArray[i] = [];
+for (let i = 0; i < 2 * n + n; i++) {
+  if (!secondaryArray[i]) secondaryArray[i] = [];
 }
 
-for (let i = 0; i < n * n; i++){
-    mainArray[i] = 2;
+for (let i = 0; i < n * n; i++) {
+  mainArray[i] = 2;
 }
 
-function makeArray() {
-    c = 0;
-    for (let i = 0; i < n; i++){
-        for (let j = 0; j < n; j++){
-            secondaryArray[i][j] = mainArray[i * n + j];
-            secondaryArray[i + n][j] = mainArray[j * n + c];
-        }
-        c++;
+function fillArray() {
+  c = 0;
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n; j++) {
+      secondaryArray[i][j] = mainArray[i * n + j];
+      secondaryArray[i + n][j] = mainArray[j * n + c];
     }
+    c++;
+  }
 
-    for (let i = 0; i < n; i++ ){
-        secondaryArray[2 * n][i] = mainArray[i * (n + 1)];
-        secondaryArray[2 * n + 1][i] = mainArray[(i + 1) * (n - 1)];
-    }
+  for (let i = 0; i < n; i++) {
+    secondaryArray[2 * n][i] = mainArray[i * (n + 1)];
+    secondaryArray[2 * n + 1][i] = mainArray[(i + 1) * (n - 1)];
+  }
 }
 
 function checkArray() {
-    for (let i = 0; i < 2 * n + 2; i++){
-        counter = 0;
-        for (let j = 1; j < n; j++){
-            if (secondaryArray[i][0] === secondaryArray[i][j] && secondaryArray[i][0] !== 2){
-                counter += 1;
-            }
-        }
-
-        if (counter === n - 1){
-            if (secondaryArray[i][0] === 1) {
-                console.log('Player with X won!');
-            } else if (secondaryArray[i][0] === 0){
-                console.log('Player with O won!');
-            }
-
-            finalTurnCheck = true;
-            $(".buttons__button-main").prop('disabled', true).css('color', 'grey');
-        }
+  for (let i = 0; i < 2 * n + 2; i++) {
+    counter = 0;
+    for (let j = 1; j < n; j++) {
+      if (secondaryArray[i][0] === secondaryArray[i][j] && secondaryArray[i][0] !== 2) {
+        counter += 1;
+      }
     }
 
-    turnCount += 1;
+    if (counter === n - 1) {
+      if (secondaryArray[i][0] === 1) {
+        $("#label_1").text('Player with X won!');
+      } else if (secondaryArray[i][0] === 0) {
+        $("#label_1").text('Player with O won!');
+      }
 
-    if (turnCount  === n * n && !finalTurnCheck){
-        $(".buttons__button-main").prop('disabled', true).css('color', 'grey');
-        console.log('Its a draw!');
+      finalTurnCheck = true;
+      $(".buttons__button-main").prop('disabled', true).css('color', 'grey');
     }
+  }
+
+  turnCount += 1;
+
+  if (turnCount === n * n && !finalTurnCheck) {
+    $(".buttons__button-main").prop('disabled', true).css('color', 'grey');
+    $("#label_1").text('Its a draw!');
+  }
 }
 
-makeArray();
+fillArray();
